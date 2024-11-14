b249f6ea52eee4414e0149bbf14a94855ed917c5
Damilare Abogunrin <43679376+St-Thierry@users.noreply.github.com>
2024-11-14 07:48:55 +0100

    Update Sidebar.jsx

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -102,12 +102,12 @@ const Sidebar = () => {
               </Box>
               <Box textAlign="center">
                 <Typography
-                  variant="h2"
+                  variant="h3"
                   color={colors.grey[100]}
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Damilare Abogunrin
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
                   VP Fancy Admin
